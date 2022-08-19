48cf018223ba11e35c6884f8ad25c011871accbc
Tom Cizeron <tom.cizeron@edhec.com>
2022-08-19 17:35:15 +0200

    First migration and creation of model Bananas

--- django_find_bananas/find_bananas/models.py
+++ django_find_bananas/find_bananas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Bananas(models.Model):
+    timestamp = models.DateTimeField()
+    nb_bananas_1 = models.IntegerField()
+    nb_bananas_2 = models.IntegerField()
+    nb_bananas_3 = models.IntegerField()
+    image_1 = models.ImageField()
+    image_2 = models.ImageField()
+    image_3 = models.ImageField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.timestamp = datetime.utcnow()
+        return super(Bananas, self).save(*args, **kwargs)
