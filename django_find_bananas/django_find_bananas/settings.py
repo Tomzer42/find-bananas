aa41609f0a46479cb15380544fc8095b5012ce66
Tom Cizeron <tom.cizeron@edhec.com>
2022-09-07 11:40:23 +0200

    change structure secret key

--- django_find_bananas/django_find_bananas/settings.py
+++ django_find_bananas/django_find_bananas/settings.py
@@ -23,7 +23,7 @@ IS_HEROKU = "DYNO" in os.environ
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU:
